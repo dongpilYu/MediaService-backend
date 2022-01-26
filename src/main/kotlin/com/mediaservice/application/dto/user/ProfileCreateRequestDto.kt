3ec5a8b58ac8582aadb0c370faab0f2c58799ddc
dongpil_yu <yoodongphil@naver.com>
2022-01-26 18:43:49 +0900

    fix: 사용하지 않는 메소드 제거

--- src/main/kotlin/com/mediaservice/application/dto/user/ProfileCreateRequestDto.kt
+++ src/main/kotlin/com/mediaservice/application/dto/user/ProfileCreateRequestDto.kt
@@ -3,16 +3,7 @@ package com.mediaservice.application.dto.user
 import com.mediaservice.domain.Profile
 
 data class ProfileCreateRequestDto(
-
     val name: String,
     val rate: String,
     val mainImage: String
-) {
-    companion object {
-        fun from(profile: Profile) = ProfileCreateRequestDto(
-            name = profile.name,
-            rate = profile.rate,
-            mainImage = profile.mainImage
-        )
-    }
-}
+)
